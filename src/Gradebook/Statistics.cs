2cbead2ae1554241f044dc67200a3df270e2cb95
Shubh More <shubham.more@klingelnberg.com>
2023-09-05 16:26:39 +0530

    Add Grade to GradeBook using switch statements

--- src/Gradebook/Statistics.cs
+++ src/Gradebook/Statistics.cs
@@ -5,12 +5,14 @@ namespace Gradebook
         public double Average; 
         public double Low; 
         public double High;
+        public char Grade;
 
-        public Statistics(double Average,double Low,double High)
+        public Statistics(double Average,double Low,double High,char Grade)
         {
             this.Average = Average;
             this.Low = Low;
             this.High = High;
+            this.Grade = Grade;
         }
     }
 }

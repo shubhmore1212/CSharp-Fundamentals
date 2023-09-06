65462a2490fabb131ad0d73e47f7b287a1553b74
Shubh More <shubham.more@klingelnberg.com>
2023-09-06 10:33:50 +0530

    Test Case for delegate and multicast delegate

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -5,8 +5,37 @@ using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 
 namespace GradeBook.Tests
 {
+
+    public delegate string WriteLogDelegate(string logMessage);
+
     public class TypeTests
     {
+        int count = 0;
+
+        [Fact]
+        public void WriteLogDelegatePointToMethod()
+        {
+            WriteLogDelegate log = ReturnMessage;
+            log += ReturnMessage;
+            log += IncrementCount;
+
+            var result = log("Hello!");
+            Assert.Equal(3, count);
+        }
+
+        string IncrementCount(string message)
+        {
+            count++;
+            return message;
+        }
+
+        string ReturnMessage(string message)
+        {
+            count++;
+            return message;
+        }
+
+
         [Fact]
         public void CSharpCanPassByRef()
         {
